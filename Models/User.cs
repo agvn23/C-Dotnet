ce281e4725a7fde4c160962525d877ef3d3a73ad
agvn23 <agvn@linuxmail.org>
2026-09-23 12:51:37 +0200

    Added Blog API

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace BlogApi.Models;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
---
+++ TimeService.cs
@@ -0,0 +1,5 @@
+// TimeService.cs
+public class TimeService
+{
+    public string Now() => DateTime.Now.ToString("T");
+}
